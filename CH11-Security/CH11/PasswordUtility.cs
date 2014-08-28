b586b8f8cdd63db9302b58a31e11f51967e4d801
BruceChen <bruce.mvc@outlook.com>
2014-08-28 21:51:49 +0800

    ASP.NET MVC 5 網站開發美學 - 第十一章 網站安全之道 範例專案

---
+++ CH11-Security/CH11/PasswordUtility.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CH11.Helpers
+{
+    public class PasswordUtility
+    {
+        public static bool PasswordLength(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            else
+            {
+                if (0 - Convert.ToInt32(Regex.IsMatch(password, "[a-z]")) -
+                       Convert.ToInt32(Regex.IsMatch(password, "[A-Z]")) -
+                       Convert.ToInt32(Regex.IsMatch(password, @"\d")) -
+                       Convert.ToInt32(Regex.IsMatch(password, ".{10,}")) <= -2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static string AESEncryptor(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(plainText);
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            string encryptedString = Convert.ToBase64String(aes.CreateEncryptor(Key, IV).TransformFinalBlock(data, 0, data.Length));
+            return encryptedString;
+        }
+
+        public static string AESDecryptor(string encryptedString, byte[] Key, byte[] IV)
+        {
+            byte[] data = Convert.FromBase64String(encryptedString);
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            string decryptedString = ASCIIEncoding.ASCII.GetString(aes.CreateDecryptor(Key, IV).TransformFinalBlock(data, 0, data.Length));
+            return decryptedString;
+        }
+
+        public static string SHA256Encryptor(string plainText)
+        {
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(plainText);
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+            byte[] result = sha256.ComputeHash(data);
+
+            return Convert.ToBase64String(result);
+        }
+
+        public static string SHA512Encryptor(string plainText)
+        {
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(plainText);
+            SHA512 sha512 = new SHA512CryptoServiceProvider();
+            byte[] result = sha512.ComputeHash(data);
+
+            return Convert.ToBase64String(result);
+        }
+
+        public static string GuidwithPassword(Guid guid, string plainText)
+        {
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(plainText + guid.ToString());
+            byte[] result;
+            SHA512Managed sha = new SHA512Managed();
+            result = sha.ComputeHash(data);
+            return Convert.ToBase64String(result);
+        }
+
+
+    }
+}
---
+++ CH11-Security/ReadMe.md
@@ -0,0 +1,15 @@
+# 第十一章 網站安全之道 範例專案 #
+
+注意：
+
+- 使用Visual Studio 2013 Update 2範本建立。
+
+此為加解密工具類別：
+
+- PasswordUtility.cs
+
+
+
+
+
+
