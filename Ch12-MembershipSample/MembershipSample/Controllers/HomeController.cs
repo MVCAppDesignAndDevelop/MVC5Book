e975a33bb8575cb4794dfdf9f79cbba2069e8daf
demofan <demo@demo.tc>
2014-08-29 11:57:07 +0800

    ASP.NET MVC 5 網站開發美學 - 第12章使用 Membership 驗證範例程式碼

---
+++ Ch12-MembershipSample/MembershipSample/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MembershipSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string account, string password)
+        {
+            if (account == "mvc" && password == "123456")
+            {
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                    //你想要存放在 User.Identy.Name 的值，通常是使用者帳號
+                    account,
+                    DateTime.Now,
+                    DateTime.Now.AddMinutes(30),
+                    true,//將管理者登入的 Cookie 設定成 Session Cookie
+                    "userdata",//userdata看你想存放啥
+                    FormsAuthentication.FormsCookiePath);
+
+                string encTicket = FormsAuthentication.Encrypt(ticket);
+
+                var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
+                cookie.HttpOnly = true;
+
+                Response.Cookies.Add(cookie);
+            }
+            return RedirectToAction("index");
+        }
+
+        [Authorize]
+        public ActionResult test()
+        {
+            return Content("登入成功");
+        }
+    }
+}
---
+++ Ch12-MembershipSample/MembershipSample/Helper/UserHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace MembershipSample.Helper
+{
+    public static class UserHelper
+    {
+        public static string GetUserData()
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                // 先取得該使用者的 FormsIdentity
+                FormsIdentity id = HttpContext.Current.User.Identity as FormsIdentity;
+                // 再取出使用者的 FormsAuthenticationTicket
+                FormsAuthenticationTicket ticket = id.Ticket;
+                var userInfo = id.Ticket.UserData;
+                return userInfo;
+            }
+            return "";
+        }
+    }
+}
