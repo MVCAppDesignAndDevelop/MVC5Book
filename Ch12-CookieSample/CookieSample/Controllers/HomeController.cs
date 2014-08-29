e1b793f661cde7cd6808e5659c5ef46d53ae5fc0
demofan <demo@demo.tc>
2014-08-29 11:56:20 +0800

    ASP.NET MVC 5 網站開發美學 - 第12章使用 Cookie 驗證範例程式碼

---
+++ Ch12-CookieSample/CookieSample/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using CookieSample.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+public ActionResult Login(string account, string password)
+{
+    var cookieName = "mvcAuth";
+    if (account == "mvc" && password == "123456")
+    {
+        if (Response.Cookies.AllKeys.Contains(cookieName))
+        {
+            var cookieVal = Response.Cookies[cookieName].Value;
+            HttpContext.Application.Remove(cookieVal);
+
+            Response.Cookies.Remove(cookieName);
+        }
+        //登入成功產生一組token
+        var token = Guid.NewGuid().ToString();
+
+        //將 token 存放到 Application 內(實務上應該存進資料庫)
+        HttpContext.Application[token] = DateTime.UtcNow.AddHours(1);
+
+        var hc = new HttpCookie(cookieName, token)
+        {
+            Expires = DateTime.Now.AddHours(1),
+            HttpOnly = true
+        };
+        Response.Cookies.Add(hc);
+    }
+    return RedirectToAction("Index");
+}
+
+        [AuthorizePlus]
+        public ActionResult Test()
+        {
+            return Content("登入成功");
+        }
+    }
+}
---
+++ Ch12-CookieSample/CookieSample/Filters/AuthorizePlusAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieSample.Filters
+{
+    public class AuthorizePlusAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var token = Convert.ToString(filterContext.HttpContext.Request.Cookies["mvcAuth"].Value);
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+
+            var loginTime = Convert.ToDateTime(filterContext.HttpContext.Application[token]);
+            if (loginTime > DateTime.UtcNow)
+            {
+                //驗證通過
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+
+        }
+    }
+}
